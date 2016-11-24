e1225a95222b0e4fe121aba0c49b60b0c3b372c8
lizhengxian <lizhengxian@yscredit.com>
2016-11-24 16:04:39 +0800

    编写第一个算法：插入排序

---
+++ src/main/java/com/lzx2005/sort/InsertionSort.java
@@ -0,0 +1,17 @@
+package com.lzx2005.sort;
+
+import com.lzx2005.data.Data;
+
+/**
+ * Created by Administrator on 2016/11/24 0024.
+ */
+public class InsertionSort {
+    public static int[] sort(int[] a){
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] sort = sort(Data.a);
+        System.out.println(sort);
+    }
+}
